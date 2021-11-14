662992c7f73f9ab1bb7404832ede41373bcc2588
Kevin Henry <kevinhenry@mac.com>
2021-11-13 23:08:58 -0800

    added djago heroku

--- pear_project/settings.py
+++ pear_project/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import django_heroku
+import django_on_heroku
+django_on_heroku.settings(locals())
 from pathlib import Path
 from datetime import timedelta
 
