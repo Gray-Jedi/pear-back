174f509e9d076befc07972a6b175c2f18009042c
Kevin Henry <kevinhenry@mac.com>
2021-11-13 23:01:39 -0800

    added djago heroku

--- pear_project/settings.py
+++ pear_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'django_heroku',
     'rest_framework',
     'api.apps.ApiConfig',
     'corsheaders',
